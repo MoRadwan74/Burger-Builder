8b36ff8bb67089e71e8c7d491c7e1605d33622c0
Radwan <mo.ahmed.radwan74@gmail.com>
2019-09-08 16:33:53 +0200

    Updating the sideDrawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer: false
+    };
+
+    sideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    SideDrawerToggleHandler = () => {
+        //this.setState({showSideDrawer: !this.state.showSideDrawer});
+        this.setState((prevState) => {
+            return{showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggled={this.SideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+export default Layout;
