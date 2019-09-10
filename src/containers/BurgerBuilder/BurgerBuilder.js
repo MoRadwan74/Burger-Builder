bde26d9b194d4cc0a55c9e8e85ceaf27d40a6f7d
Radwan <mo.ahmed.radwan74@gmail.com>
2019-09-10 13:05:47 +0200

    Adding spinner while loging the order to the server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilliary/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -12,40 +15,41 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 }
 
-class BurgerBuilder extends Component{
-    state={
-        ingredients:{
-            salad:0,
-            bacon:0,
-            cheese:0,
-            meat:0
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     };
 
-    updatePurchaseState (ingredients){
+    updatePurchaseState(ingredients) {
         // eslint-disable-next-line
         const sum = Object.keys(ingredients)
-            .map(igKey =>{
+            .map(igKey => {
                 return ingredients[igKey];
             })
             .reduce((sum, el) => {
                 return sum + el
             }, 0);
-            // We use zero to indicate we don't add an outside value on the sum
-        
+        // We use zero to indicate we don't add an outside value on the sum
+
         // At the end, sum either will be zero indicating no ingredients added or any other value representing the number of ingredients.
 
-        this.setState({purchasable: sum > 0});
+        this.setState({ purchasable: sum > 0 });
         // This will either true or false based on sum > 0 or not.
     }
 
-    addIngredientHandler = (type) =>{
+    addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
-        const updatedIngredients ={
+        const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
@@ -63,15 +67,15 @@ class BurgerBuilder extends Component{
         this.updatePurchaseState(updatedIngredients);
     }
 
-    removeIngredientHandler = (type) =>{
+    removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
 
         // Condition to check if the array "transformedIngredients" is zero or less
-        if (oldCount <= 0){
+        if (oldCount <= 0) {
             return;
         }
         const updatedCount = oldCount - 1;
-        const updatedIngredients ={
+        const updatedIngredients = {
             ...this.state.ingredients
         };
         updatedIngredients[type] = updatedCount;
@@ -95,36 +99,65 @@ class BurgerBuilder extends Component{
         });
     }
 
-    purchaseCancelHandler = () =>{
-        this.setState({purchasing:false});
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
     }
 
-    purchaseContinueHandler = () =>{
-        alert('You continue!');
+    purchaseContinueHandler = () => {
+        //alert('You continue!');
+        this.setState({ loading: true });
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,    // We should calculate the total price on the server.
+            // Some dummy order data (Later, we will add a form to submit this data by the user.)
+            customer: {
+                name: 'Mohamed Radwan',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'Egypt'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+        Axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false, purchasing: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
     }
 
-    render(){
+    render() {
         const disabledInfo = {
             ...this.state.ingredients
         };
         // eslint-disable-next-line
-        for(let key in disabledInfo){   
-            disabledInfo[key] = disabledInfo[key] <= 0  
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
             // disabledInfo[key] should be true or false, if it's true(meaning no ingredients), we should disable.
         }
         // {salad: true, meat: false, ...}
-        return(
+
+        let orderSummary = <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice} />;
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
+        }
+
+        return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}
-                        price={this.state.totalPrice}/>
+                    {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls 
-                    ingredientAdded={this.addIngredientHandler} 
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
